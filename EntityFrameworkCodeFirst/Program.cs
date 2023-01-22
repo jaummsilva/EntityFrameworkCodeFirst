3875aa3048919e286e3b09d04000ace0ea44c8d5
João <Joo@DESKTOP-PC02EDM>
2023-01-22 14:21:34 -0300

    Adicionar arquivos de projeto.

---
+++ EntityFrameworkCodeFirst/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ClienteContext())
+            {
+                var quantidade_clientes = context.Clientes.Count();
+                context.Clientes.Add(new Cliente() { Nome = "João", Telefone = "984803109" });
+                context.SaveChanges();
+
+                context.Produtos.Add(new Produto() { Nome = "Camera 2X", Descricao = "Outra tabela" });
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
